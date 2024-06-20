a11f92a558e5244385f3c9e50e84cdee36e61b56
Kazamaki76 <kazamaki76@gmail.com>
2024-06-21 00:23:57 +0700

    add mongoose

--- server/config.js
+++ server/config.js
@@ -1,1 +1,2 @@
 export const PORT = 5555;
+export const mongoURL = 'mongodb+srv://root:haupcar2024@cluster0.c3gc3f8.mongodb.net/cars?retryWrites=true&w=majority&appName=Cluster0'
--- server/index.js
+++ server/index.js
@@ -1,9 +1,23 @@
 import express from "express";
-import { PORT } from "./config.js";
+import { PORT ,mongoURL} from "./config.js";
+import mongoose from "mongoose";
 
 
 const app = express();
 
+app.get("/", (req, res) => {
+    console.log(req);
+    return res.status(234).send("Hello World");
+})
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
+
+mongoose
+    .connect(mongoURL)
+    .then(() => {
+        console.log("Database Connected");
+    })
+    .catch((err) => {
+        console.log(err);
+    })
