387925edd16692d9de4bba4a8018ceda14ad46fa
Kazamaki76 <kazamaki76@gmail.com>
2024-06-21 16:53:53 +0700

    add modal

--- client/front/src/components/home/CarSingleCard.jsx
+++ client/front/src/components/home/CarSingleCard.jsx
@@ -1,11 +1,15 @@
 import { Link } from "react-router-dom";
 import { PiBookOpenTextLight } from "react-icons/pi";
-import { BiUserCircle } from "react-icons/bi";
+import { BiUserCircle, BiShow } from "react-icons/bi";
 import { AiOutlineEdit } from "react-icons/ai";
 import { BsInfoCircle } from "react-icons/bs";
 import { MdOutlineDelete } from "react-icons/md";
+import { useState } from "react";
+import CarModal from "./CarModal";
 
 const CarSingleCard = ({ car }) => {
+  const [showModal, setShowModal] = useState(false);
+
   return (
     <div
       key={car._id}
@@ -14,16 +18,20 @@ const CarSingleCard = ({ car }) => {
       <h2 className="absolute top-1 right-2 px-4 py-1 bg-red-300 rounded-lg">
         {car.brand}
       </h2>
-      <h4 className="my-2 text-gray-500">{car.licensePlate}</h4>
+      <h4 className="my-2 text-gray-500"> License Plate : {car.licensePlate}</h4>
       <div className="flex justify-start items-center gap-x-2">
         <PiBookOpenTextLight className="text-red-300 text-2xl" />
-        <h2 className="my-1">{car.remarks}</h2>
+        <h2 className="my-1">Model : {car.model}</h2>
       </div>
       <div className="flex justify-start items-center gap-x-2">
-        <div className="text-red-300 text-2xl" />
-        <h2 className="my-1">{car.model}</h2>
+        <PiBookOpenTextLight className="text-red-300 text-2xl" />
+        <h2 className="my-1">Remark : {car.remarks}</h2>
       </div>
       <div className="flex justify-between items-center gap-x-2 mt-4 p-4">
+      <BiShow
+        className="text-3xl text-blue-800 hover:text-black cursor-pointer"
+        onClick={() => setShowModal(true)}
+      />
         <Link to={`/cars/details/${car._id}`}>
           <BsInfoCircle className="text-2xl text-green-800 hover:text-black" />
         </Link>
@@ -34,6 +42,7 @@ const CarSingleCard = ({ car }) => {
           <MdOutlineDelete className="text-2xl text-red-600 hover:text-black" />
         </Link>
       </div>
+      {showModal && <CarModal car={car} onClose={() => setShowModal(false)} />}
     </div>
   );
 };
